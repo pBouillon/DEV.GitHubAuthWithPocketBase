e03d31ee98a2b715417946606d8647c210db757b
pBouillon <pro.pierre.bouillon@protonmail.com>
2024-08-14 09:27:45 +0200

    chore: setup styles and packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  template: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'DEV.GitHubAuthWithPocketBase';
-}
+export class AppComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideExperimentalZonelessChangeDetection()],
+}).catch((err) => console.error(err));
